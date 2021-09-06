d1d7f0dfffd5a7dfa8b21d9079dbc19664ed3115
Gandalfr22 <frkn_yslbg@hotmail.com>
2021-09-06 19:31:52 +0300

    didnt wok

--- helpers/connect.js
+++ helpers/connect.js
@@ -1,15 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = () => {
- mongoose.connect("mongodb+srv://admin:Admin123@myapp.oux5y.mongodb.net/newapp?retryWrites=true&w=majority",{
- server: {
-   socketOptions: {
-     socketTimeoutMS: 0,
-     connectionTimeout: 0
-   }
- }
-
- });
+ mongoose.connect("mongodb+srv://admin:Admin123@myapp.oux5y.mongodb.net/newapp?retryWrites=true&w=majority");
 }
 
 
