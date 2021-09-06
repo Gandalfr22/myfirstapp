7dc58d4c656e2ff8f1eeedd029fbdb106572a726
Gandalfr22 <69119630+Gandalfr22@users.noreply.github.com>
2021-09-06 16:36:22 +0300

    Update connect.js

--- helpers/connect.js
+++ helpers/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports = () => {
 const uri =
-"mongodb+srv://admin:Admin123@myapp.oux5y.mongodb.net/newapp?retryWrites=true&w=majority"
+"mongodb+srv://admin:xxx@myapp.oux5y.mongodb.net/newapp?retryWrites=true&w=majority"
 async function run() {
   await   mongoose.connect(uri)
   }
