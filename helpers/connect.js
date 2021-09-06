995850532886daead73b820723575fccaa04ddbc
Gandalfr22 <frkn_yslbg@hotmail.com>
2021-09-06 19:27:42 +0300

    fix timeout

--- helpers/connect.js
+++ helpers/connect.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
 
 module.exports = () => {
- mongoose.connect("mongodb+srv://admin:Admin123@myapp.oux5y.mongodb.net/newapp?retryWrites=true&w=majority");
+ mongoose.connect("mongodb+srv://admin:Admin123@myapp.oux5y.mongodb.net/newapp?retryWrites=true&w=majority",{
+ server: {
+   socketOptions: {
+     socketTimeoutMS: 0,
+     connectionTimeout: 0
+   }
+ }
+
+ });
 }
 
 
